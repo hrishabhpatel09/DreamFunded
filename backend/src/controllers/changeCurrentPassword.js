b7b8d364344cb2455b6140b67103a84dfedf1160
silent-killer132 <143581664+silent-killer132@users.noreply.github.com>
2024-06-25 19:11:30 +0530

    Comment removed from 4 newly created controllers

--- backend/src/controllers/changeCurrentPassword.js
+++ backend/src/controllers/changeCurrentPassword.js
@@ -7,13 +7,12 @@ export const changeCurrentPassword =asyncHandler(async(req,res)=>{
     const {oldPassword , newPassword }=req.body
 
     const user = await User.findById(req.user?._id)
-   const isPasswordCorrect=await user.isPasswordCorrect(oldPassword) // since we used async while declaring this method in user.models.js this means 
-//    it will take some time therefore need to use await here
-// isPasswordCorrect this is custom schema mehtod declared in uer.models.js
+   const isPasswordCorrect=await user.isPasswordCorrect(oldPassword) 
+
    if(!isPasswordCorrect){
     throw new ApiError(400, "Invalid old password")
        }
-       user.password = newPassword// this sets old password to new passwprd
+       user.password = newPassword
        await user.save({validateBeforeSave:false})
        
        return res.status(200).json(new ApiResponse( "Password changed successfully", {}))
