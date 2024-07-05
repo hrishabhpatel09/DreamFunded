e24b8d809283e8ceb0a8bef1435d2f0b375cc371
Hrishabh Patel <hrishabhpatel2023@gmail.com>
2024-07-05 16:23:25 +0530

    Login Page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Routes,Route,RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Login from './Pages/Login.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router}>
     <App />
-  </React.StrictMode>,
+    </RouterProvider>
+  </React.StrictMode>
 )
